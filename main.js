f05cb2a19dee60b020f55a98a6693ffb7d375748
JonCass <JCass1980@gmail.com>
2017-03-15 13:44:25 -0700

    needs to update every second

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ $(document).ready(function(){
 	var rawDate = new Date()
 	var clockOutput = ""
 	var hours =""
-
+	
 	hours = rawDate.getHours()
-
-	$("#clock").html(hours)
+	hours = 0 ? 12 : hours
+	hours = (hours > 12) ? hours - 12 : hours
+	clockOutput = hours +":"+ rawDate.getMinutes() +":"+ rawDate.getSeconds()
+	
+	$("#clock").html(clockOutput)
 })

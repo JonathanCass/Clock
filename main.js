9a214c008d28cd2cb7fee5a23e1ab719bb29812d
JonCass <JCass1980@gmail.com>
2017-03-15 14:21:15 -0700

    Clock Version 1.0

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
-	var rawDate = new Date()
-	var clockOutput = ""
-	var hours =""
+	var runClock = setInterval (clock,1000)
 	
-	hours = rawDate.getHours()
-	hours = 0 ? 12 : hours
-	hours = (hours > 12) ? hours - 12 : hours
-	clockOutput = hours +":"+ rawDate.getMinutes() +":"+ rawDate.getSeconds()
-	
-	$("#clock").html(clockOutput)
+	function clock(){
+		var rawDate = new Date()
+		var hours = rawDate.getHours()
+		hours = 0 ? 12 : hours
+		hours = (hours > 12) ? hours - 12 : hours
+		var minutes = rawDate.getMinutes()
+		minutes = (minutes < 10) ? "0" + minutes : minutes
+		var seconds = rawDate.getSeconds()
+		seconds = (seconds < 10) ? "0" + seconds : seconds
+		var clockOutput = hours +":"+ minutes +":"+ seconds
+		$("#clock").html(clockOutput)
+	}
 })
